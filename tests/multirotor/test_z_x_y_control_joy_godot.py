07bc000f90843fb9eb92da4a5f7174c61da6f875
Corrado Santoro <santoro@dmi.unict.it>
2024-05-20 12:45:10 +0200

    aggiuntio controllo remoto

--- tests/multirotor/test_z_x_y_control_joy_godot.py
+++ tests/multirotor/test_z_x_y_control_joy_godot.py
@@ -10,7 +10,7 @@ from lib.data.plot import DataPlotter
 from lib.godot.interface import *
 
 import math
-
+import pygame
 
 
 class MultirotorRobot:
@@ -21,15 +21,29 @@ class MultirotorRobot:
                                  5, True)
         self.z_control = PIDSat(2.0, 0.0, 0.0, 2)  # 2 m/s
 
-        self.w_roll_control = PIDSat(0.2, 0.5, 0.0, 2, True)
+        self.w_roll_control = PIDSat(0.5, 1.0, 0.02, 1, True)
         self.roll_control = PIDSat(1.0, 0.0, 0.0, 2, True) # max 2 rad/s
 
-        self.w_pitch_control = PIDSat(0.2, 0.5, 0.0, 2, True)
+        self.w_pitch_control = PIDSat(0.5, 1.0, 0.02, 1, True)
         self.pitch_control = PIDSat(1.0, 0.0, 0.0, 2, True) # max 2 rad/s
 
-        self.z_target = 1.0
-        self.roll_target = math.radians(2.5)
+        self.y_control = PIDSat(1.5, 0.0, 0.0,
+                                 2.0, True)
+        self.vy_control = PIDSat(0.1, 0.1, 0.5,
+                                 math.radians(40), True)
+
+        self.x_control = PIDSat(1.5, 0.0, 0.0,
+                                 2.0, True)
+        self.vx_control = PIDSat(0.1, 0.1, 0.5,
+                                 math.radians(40), True)
+
+        self.z_target = 1.5
+        self.roll_target = 0.0 #math.radians(2.5)
         self.pitch_target = 0.0 #math.radians(5)
+        self.xy_target = 0.0
+        self.x_target = 0.0
+        self.vy_target = 0.0
+        self.y_target = 0.0
 
         self.f = 0
         self.f_roll = 0
@@ -58,6 +72,14 @@ class MultirotorRobot:
         vz_target = self.z_control.evaluate(self.delta_t, self.z_target, z)
         self.f = self.vz_control.evaluate(self.delta_t, vz_target, vz)
 
+        # x + vx control
+        self.vx_target = self.x_control.evaluate(self.delta_t, self.x_target, x)
+        self.pitch_target = self.vx_control.evaluate(self.delta_t, self.vx_target, vx)
+
+        # y + vy control
+        self.vy_target = self.y_control.evaluate(self.delta_t, self.y_target, y)
+        self.roll_target = - self.vy_control.evaluate(self.delta_t, self.vy_target, vy)
+
         # roll control
         self.w_roll_target = self.roll_control.evaluate(self.delta_t, self.roll_target, roll)
         self.f_roll = self.w_roll_control.evaluate(self.delta_t, self.w_roll_target, w_roll)
@@ -71,24 +93,50 @@ class MultirotorRobot:
         self.plot.add('w_roll_target', self.w_roll_target)
         self.plot.add('roll', roll)
         self.plot.add('roll_target', self.roll_target)
+        self.plot.add('w_pitch', w_pitch)
+        self.plot.add('w_pitch_target', self.w_pitch_target)
+        self.plot.add('pitch', pitch)
+        self.plot.add('pitch_target', self.pitch_target)
         self.plot.add('vy', vy)
+        self.plot.add('vx', vx)
 
-        if self.t >= 10:
-            self.plot.plot(['t', 'time'],
-                           [['w_roll', 'w_roll'], ['w_roll_target', 'w_roll_target'] ])
-            self.plot.plot(['t', 'time'],
-                           [['roll', 'roll'], ['roll_target', 'roll_target'] ])
-            self.plot.plot(['t', 'time'],
-                           [['vy', 'vy'] ])
-            self.plot.show()
-            return False
+        return True
+
+
+class Joy:
+
+    def __init__(self):
+        pygame.init()
+        self.j = pygame.joystick.Joystick(0)
+        self.j.init()
+
+
+    def rescale(self, v):
+        if v > 0.5:
+            return 1
+        elif v < -0.5:
+            return -1
         else:
-            return True
+            return 0
 
+
+    def remote(self, robot):
+        pygame.event.pump()
+        left_h = self.rescale(self.j.get_axis(0))
+        left_v = self.rescale(self.j.get_axis(1))
+
+        right_h = self.rescale(self.j.get_axis(3))
+        right_v = self.rescale(self.j.get_axis(4))
+
+        robot.z_target += (-left_v) * 0.01
+        robot.x_target += right_v * 0.01
+        robot.y_target += right_h * 0.01
 
 
 if __name__ == '__main__':
     robot = MultirotorRobot()
+    j = Joy()
     while robot.run():
-        pass
+        j.remote(robot)
+        print(robot.x_target, ' ', robot.y_target, ' ', robot.z_target)
 
