b6d7579d1f4af2247797da99c434d11d3614c426
Corrado Santoro <santoro@dmi.unict.it>
2025-03-24 10:13:27 +0100

    position control added

---
+++ lib/system/controllers.py
@@ -0,0 +1,50 @@
+#
+# controllers.py
+#
+
+from lib.system.basic import *
+from typing import Optional
+
+def saturate(inp: float, sat: float) -> tuple[float, bool]:
+    if inp > sat:
+        return (sat, True)
+    if inp < - sat:
+        return (-sat, True)
+    return (inp, False)
+
+
+class P_Controller:
+
+    def __init__(self, _kp: float, _sat: Optional[float] = None):
+        self.kp = _kp
+        self.saturation = _sat
+
+    def evaluate(self, delta_t: float, _error: float) -> float:
+        out = self.kp * _error
+        if self.saturation is not None:
+            out, _ = saturate(out, self.saturation)
+        return out
+
+
+class PI_Controller:
+
+    def __init__(self, _kp: float, _ki: float, _sat: Optional[float] = None):
+        self.kp = _kp
+        self.ki = _ki
+        self.i = Integrator()
+        self.saturation = _sat
+        self.in_saturation = False
+
+    def evaluate(self, delta_t: float, _error: float) -> float:
+        out = self.kp * _error
+
+        if self.in_saturation:
+            out = out + self.ki * self.i.prev_output
+        else:
+            out = out + self.ki * self.i.evaluate(delta_t, _error)
+
+        if self.saturation is not None:
+            out, self.in_saturation = saturate(out, self.saturation)
+
+        return out
+
