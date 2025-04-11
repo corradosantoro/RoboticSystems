68cf0b1fb70f98882513819d2ce05aacbaf7a3e2
Corrado Santoro <santoro@dmi.unict.it>
2025-04-11 12:00:19 +0200

    updates

--- lib/utils/geometry.py
+++ lib/utils/geometry.py
@@ -12,6 +12,20 @@ def normalize_angle(a):
     return a
 
 
+def rototranslate(xp, yp, xc, yc, t):
+    """
+    Rototranslate a point from a reference system (x',y') to a system (x,y)
+    :param xp,yp: The point in the reference system (x', y')
+    :param xc, yc: The origin of (x',y') in (x,y) coordinates
+    :param t: The rotation of x' with respect to x (in radians)
+    """
+    cos_t = math.cos(t)
+    sin_t = math.sin(t)
+    global_point_x = xc + xp * cos_t - yp * sin_t
+    global_point_y = yc + xp * sin_t + yp * cos_t
+    return global_point_x, global_point_y
+
+
 def global_to_local(xc, yc, t, x, y):
     cos_t = math.cos(t)
     sin_t = math.sin(t)
